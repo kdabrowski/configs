6c0c9e25eb1e40f6333c8c8b497df8a32a6b5850
Krzysztof Dabrowski <biz@op.pl>
2025-06-02 15:58:27 +0200

    Add configs

---
+++ nvim/lua/bizsetupfile/init.lua
@@ -0,0 +1,10 @@
+require('bizsetupfile.remap')
+require('bizsetupfile.packer')
+require('bizsetupfile.set')
+require('leap').create_default_mappings()
+
+vim.o.background = "dark" -- or "light" for light mode
+
+vim.cmd([[colorscheme gruvbox]])
+
+
---
+++ nvim/lua/bizsetupfile/packer.lua
@@ -0,0 +1,48 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use { "ellisonleao/gruvbox.nvim" }
+
+  use('nvim-lua/plenary.nvim')
+  use('tpope/vim-fugitive')
+  use('mbbill/undotree')
+  use('mogulla3/rspec.nvim')
+  use('ggandor/leap.nvim')
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment these if you want to manage LSP servers from neovim
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  }
+  use {
+	 "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+  }
+end)
