5cf21c00ba4130b75993ad187f8399caa7386024
Krzysztof Dabrowski <biz@op.pl>
2025-07-18 11:39:56 +0200

    Delete nvim directory

--- nvim/lua/bizsetupfile/remap.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = ' '
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("n", "<leadr>TT", function() require("neotest").run.run() end)
-vim.keymap.set("n", "<leadr>TF", function() require("neotest").run.run(vim.fn.expand("%")) end)
